e092ecf8dbf65e2b3e88d61b4f0c599a16377a3b
michael vaysman <mvaysman@gmail.com>
2023-03-06 00:45:55 -0500

    modified db and added data loader

---
+++ Tables/pgon.DailySnapshotPrices.sql
@@ -0,0 +1,29 @@
+﻿CREATE TABLE [pgon].[DailySnapshotPrices]
+(
+	[DailySnapshotPricesId] INT NOT NULL PRIMARY KEY identity(1,1), 
+    [Ticker] VARCHAR(50)NOT NULL, 
+    [Date] DATE NOT NULL, 
+    [Volume] FLOAT NULL, 
+    [VolumeWeighted] FLOAT NULL, 
+    [Open] FLOAT NULL, 
+    [Close] FLOAT NULL, 
+    [High] FLOAT NULL, 
+    [Low] FLOAT NULL, 
+    [Time] BIGINT NULL, 
+    [Transactions] INT NULL, 
+    [TransactionsDollars] FLOAT NULL, 
+    [OpenCloseChange] FLOAT NULL, 
+    [OpenCloseChangeRank] INT NULL, 
+    [TransactionsRank] INT NULL, 
+    [TransactionsDollarsRank] INT NULL, 
+    [UpdatedOn] DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    
+)
+
+GO
+
+CREATE INDEX [IX_DailySnapshotPrices_Column] ON [pgon].[DailySnapshotPrices] ([Ticker],[Date])
+
+GO
+
+CREATE INDEX [IX_DailySnapshotPrices_Date] ON [pgon].[DailySnapshotPrices] ([Date])
