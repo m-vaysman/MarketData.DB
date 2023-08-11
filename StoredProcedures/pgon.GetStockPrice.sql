feb73d46a0562ee4f36c22383d84840c351e4171
michael vaysman <mvaysman@gmail.com>
2023-08-10 23:05:13 -0400

    more work

---
+++ StoredProcedures/pgon.GetStockPrice.sql
@@ -0,0 +1,5 @@
+﻿CREATE PROCEDURE [pgon].[GetStockPrice]
+	@ticker varchar(max),
+	@date DateTime
+AS
+	select Ticker, Price, [TimeStamp] from pgon.DailyTimeSeriesPrices where Ticker=@ticker and [TimeStamp]>@date and [TimeStamp]< DATEADD(DAY,1,@date)
---
+++ Tables/pgon.DailyTimeSeriesVolumes.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [pgon].[DailyTimeSeriesVolumes]
+(
+    [Id] BIGINT NOT NULL PRIMARY KEY identity(1,1),
+	[Ticker] varchar(50) not null,
+	[Volume] float not null,
+	[VolumeWeighted] float not null,
+	[AverageVolume] float not null,
+	[TimeStamp] DateTime not null
+)
+
+
+
+
+GO
+
+CREATE INDEX [IX_DailyTimeSeriesVolumes_Column] ON [pgon].[DailyTimeSeriesVolumes] ([Ticker]) include (Volume,VolumeWeighted,AverageVolume,[TimeStamp])
+
+GO
+CREATE INDEX [IX_DailyTimeSeriesVolumes_Column3] ON [pgon].[DailyTimeSeriesVolumes] ([TimeStamp]) include (Ticker,Volume,VolumeWeighted,AverageVolume)
+
+GO
+CREATE INDEX [IX_DailyTimeSeriesVolumes_Column2] ON [pgon].[DailyTimeSeriesVolumes] ([Ticker],[TimeStamp]) include (Volume,VolumeWeighted,AverageVolume)
