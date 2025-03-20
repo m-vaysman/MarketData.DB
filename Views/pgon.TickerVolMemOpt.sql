488e014dbf441a71d1f011387fbcb233aa99ffdc
Michael Vaysman <mvaysman@gmail.com>
2025-03-20 03:33:18 -0700

    more changes

---
+++ Views/pgon.TickerVolMemOpt.sql
@@ -0,0 +1,12 @@
+﻿CREATE VIEW [pgon].[TickerVolatilityMemOpt]
+	AS 
+	
+  select *
+   ,STDEV(dpa.[Return]) OVER (partition by dpa.ticker ORDER BY dpa.Date  ROWS BETWEEN 251 PRECEDING AND CURRENT ROW) AS AnnualStDev
+   ,STDEV(dpa.[Return]) OVER (partition by dpa.ticker ORDER BY dpa.Date  ROWS BETWEEN 125 PRECEDING AND CURRENT ROW) AS SemiAnnualStDev
+      ,STDEV(dpa.[Return]) OVER (partition by dpa.ticker ORDER BY dpa.Date  ROWS BETWEEN 62 PRECEDING AND CURRENT ROW) AS QtrAnnualStDev
+    ,STDEV(dpa.[Return]) OVER (partition by dpa.ticker ORDER BY dpa.Date  ROWS BETWEEN 20 PRECEDING AND CURRENT ROW) AS MthAnnualStDev
+   FROM pgon.DailySnapshotPricesMemOpt as dpa
+
+
+	
